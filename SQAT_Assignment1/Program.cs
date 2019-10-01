0675c4911fbc490a167a21ecd552f6c88e6f8c8e
Hitesh Naidu <hnaidu9764@conestogac.on.ca>
2019-10-01 00:33:31 -0400

    Add project files.

---
+++ SQAT_Assignment1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SQAT_Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte choice;
+            string stringChoice;
+            do
+            {
+                Console.WriteLine("1. Enter triangle dimensions");
+                Console.WriteLine("2. Exit");
+                stringChoice = Console.ReadLine();
+            } while (byte.TryParse(stringChoice, out choice) && (choice == 1 || choice == 2));
+            if (choice == 1)
+            {
+                Console.WriteLine("Please enter 3 integers");
+                string input1 = Console.ReadLine();
+                string input2 = Console.ReadLine();
+                string input3 = Console.ReadLine();
+                int.TryParse(input1, out int integer1);
+                int.TryParse(input2, out int integer2);
+                int.TryParse(input3, out int integer3);
+                string finalOutput = TriangleSolver.Analyze(integer1, integer2, integer3);
+            }
+            else if (choice == 2)
+                return;
+        }
+    }
+}
---
+++ SQAT_Assignment1/TriangleSolver.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace SQAT_Assignment1
+{
+    public static class TriangleSolver
+    {
+        public static string Analyze(int integer1, int integer2, int integer3)
+        {
+            if (integer1 <= 0 || integer2 <= 0 || integer3 <= 0)
+                return "Unable to form a triangle";
+            else if ((integer1 == integer2 && integer1 != integer3) || (integer1 == integer3 && integer1 != integer2)
+                || (integer2 == integer3 && integer2 != integer1))
+                return "Isosceles triangle formed";
+            else if (integer1 == integer2 && integer2 == integer3)
+                return "Equilateral triangle formed";
+            else if (integer1 != integer2 && integer2 != integer3 && integer3 != integer1)
+                return "Scalene triangle formed";
+            else
+                return "Unable to form a triangle";
+        }
+    }
+
+    [TestFixture]
+    class TestCase
+    {
+        [TestCase]
+        public void TestCase1()
+        {
+            Assert.AreEqual("Unable to form a triangle", TriangleSolver.Analyze(0, 0, 0));
+        }
+
+        [TestCase]
+        public void TestCase2()
+        {
+            Assert.AreEqual("Isosceles triangle formed", TriangleSolver.Analyze(2, 2, 1));
+        }
+
+        [TestCase]
+        public void TestCase3()
+        {
+            Assert.AreEqual("Equilateral triangle formed", TriangleSolver.Analyze(2, 2, 2));
+        }
+
+        [TestCase]
+        public void TestCase4()
+        {
+            Assert.AreEqual("Scalene triangle formed", TriangleSolver.Analyze(2, 3, 5));
+        }
+
+        [TestCase]
+        public void TestCase5()
+        {
+            Assert.AreEqual("Unable to form a triangle", TriangleSolver.Analyze(-1, -1, -1));
+        }
+
+        [TestCase]
+        public void TestCase6()
+        {
+            Assert.AreEqual("Equilateral triangle formed", TriangleSolver.Analyze(5, 5, 5));
+        }
+
+        [TestCase]
+        public void TestCase7()
+        {
+            Assert.AreEqual("Isosceles triangle formed", TriangleSolver.Analyze(5, 5, 4));
+        }
+
+        [TestCase]
+        public void TestCase8()
+        {
+            Assert.AreEqual("Scalene triangle formed", TriangleSolver.Analyze(5, 6, 4));
+        }
+    }
+}
